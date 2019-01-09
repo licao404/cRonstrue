13b8f444a5f11b649f1975defb7530a92224d66a
李操 <lc195631@alibaba-inc.com>
2019-01-09 16:51:34 +0800

    feat: just export zh_CN pkg

---
+++ zh_CN.js
@@ -0,0 +1,5 @@
+// This file allows dist/cronstrue-i18n.js to be required from Node as:
+// var cronstrue = require('cronstrue/i18n');
+
+var cronstrueWithZh_CN = require("./dist/cronstrue-zh_CN.js");
+module.exports = cronstrueWithZh_CN;
